5e74017ac41025e335ae5f721bf592f2d596fd6d
How <How@TrustONE.com.tw>
2024-04-11 12:14:16 +0800

    migration!

---
+++ Migrations/20240410095223_ModFieldAttributes.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myWebProj1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModFieldAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pizzas",
+                table: "Pizzas");
+
+            migrationBuilder.RenameTable(
+                name: "Pizzas",
+                newName: "PizzaMenu");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "PizzaMenu",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PizzaMenu",
+                table: "PizzaMenu",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PizzaMenu",
+                table: "PizzaMenu");
+
+            migrationBuilder.RenameTable(
+                name: "PizzaMenu",
+                newName: "Pizzas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pizzas",
+                table: "Pizzas",
+                column: "Id");
+        }
+    }
+}
---
+++ Migrations/20240411021807_ModDatabaseName_2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myWebProj1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModDatabaseName_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
